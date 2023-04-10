4543bc275aa167512415b446ac07009da46471b8
eaplicano <eliud.aplicano@unah.hn>
2023-04-10 13:28:24 -0600

    Login

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,29 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const passport = require('passport');
-const { isLoggedIn } = require('../lib/auth');
-const session = require('express-session');
-const { Session } = require('express-session');
-const { Passport } = require('passport');
+const passport = require("passport");
+const { isLoggedIn } = require("../lib/auth");
+const session = require("express-session");
+const { Session } = require("express-session");
+const { Passport } = require("passport");
 // const passport1 = require('./src/lib/passport.js')
 // const validar = passport.activo;
 // const activo = passport.va
 
-
 // SIGNUP
-router.get('/signup', (req, res) => {
-  res.render('auth/signup');
+router.get("/signup", (req, res) => {
+  res.render("auth/signup");
 });
 
-router.post('/signup', passport.authenticate('local.signup', {
-  successRedirect: '/profile',
-  failureRedirect: '/signup',
-  failureFlash: true
-}));
+router.post(
+  "/signup",
+  passport.authenticate("local.signup", {
+    successRedirect: "/profile",
+    failureRedirect: "/signup",
+    failureFlash: true,
+  })
+);
 
 // SINGIN
-router.get('/signin', (req, res) => {
+router.get("/signin", (req, res) => {
   req.sessionID;
   // console.log(req.sessionID);
   // passport.validacioncodigo()
@@ -32,7 +34,7 @@ router.get('/signin', (req, res) => {
   //   res.redirect('profile');
   // }
   // else{
-    res.render('auth/signin');
+  res.render("auth/signin");
   // }
 });
 // router.get('/signin', isLoggedIn, (req, res) => {
@@ -42,24 +44,24 @@ router.get('/signin', (req, res) => {
 //   else{
 //     res.render('auth/signin');
 //   }
-  
-  // console.log(isLoggedIn);
-  
+
+// console.log(isLoggedIn);
+
 // });
 
-router.post('/signin', (req, res, next) => {
-  req.check('USUARIO', 'Username is Required').notEmpty();
-  req.check('CONTRASEÑA', 'Password is Required').notEmpty();
+router.post("/signin", (req, res, next) => {
+  req.check("USUARIO", "Username is Required").notEmpty();
+  req.check("CONTRASEÑA", "Password is Required").notEmpty();
   const errors = req.validationErrors();
   if (errors.length > 0) {
-    req.flash('message', errors[0].msg);
-    res.redirect('/signin');
+    req.flash("message", errors[0].msg);
+    //res.redirect("/signin");
   }
-  // console.log(passport.authenticate('local.signin'));
-  passport.authenticate('local.signin', {
-    successRedirect: '/profile',
-    failureRedirect: '/signin',
-    failureFlash: true
+  //console.log(passport.authenticate("local.signin"));
+  passport.authenticate("local.signin", {
+    successRedirect: "/profile",
+    failureRedirect: "/signin",
+    failureFlash: true,
   })(req, res, next);
   // if( passport.authenticate('local.signin')){
   //   console.log('ingreso')
@@ -67,13 +69,13 @@ router.post('/signin', (req, res, next) => {
   // }
 });
 
-router.get('/logout', (req, res) => {
+router.get("/logout", (req, res) => {
   req.logOut();
-  res.redirect('/');
+  res.redirect("/");
 });
 
-router.get('/profile', isLoggedIn, (req, res) => {
-  res.render('profile');
+router.get("/profile", isLoggedIn, (req, res) => {
+  res.render("profile");
 });
 
 module.exports = router;
